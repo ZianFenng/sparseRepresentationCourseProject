83a41f51fd14b29a441ab2cfa75fa8fda11167d9
ZianFenng <LeonGong@dyn-160-39-133-166.dyn.columbia.edu>
2017-04-13 22:08:25 -0400

    training set has 54 point per class, the rest 10 points are test data prediction correct rate is above 96%

--- robust_version.py
+++ robust_version.py
@@ -15,10 +15,11 @@ import time
 # Parameters of the images
 DIM_1 = 192
 DIM_2 = 168
+REDUCEDDIMENSION = 100
 # Dense Noise
 Epsilon = 1000
 CLASSES = 13
-SAMPLE_EACH_CLASS = 62
+SAMPLE_EACH_CLASS = 54
 DOWNSAMPLE_COEFFICIENT = 4
 
 # Find the sparse solution of SOCP
@@ -35,7 +36,7 @@ def SOCP(y, A, Epsilon):
     start = time.time()
 
     prob = cvx.Problem(obj, constraints)
-    prob.solve(solver='SCS', max_iters = 5, verbose = True)
+    prob.solve(solver='SCS', max_iters = 5, verbose = False)
 
     finish = time.time()
 
@@ -43,14 +44,14 @@ def SOCP(y, A, Epsilon):
 
     # print "Status:", prob.status 
 
-    print "Optimal value with SCS", prob.value 
+    # print "Optimal value with SCS", prob.value 
 
 
     return x.value
 
 def LoadImage():
     # Current Path
-    currPath = '/Users/LeonGong/Desktop/ELEN6886/CroppedYale//'
+    currPath = '/Volumes/Myspace/Courses/sparse/project/Code-Lecture1/FaceIntroDemo/CroppedYale/'
     # Load the first image
     X_train= []
 
@@ -69,7 +70,7 @@ def LoadImage():
         fileList = glob.glob("*.pgm")
         # Class i
         # Exculde 
-        for file_item in fileList[2:]:
+        for file_item in fileList[10:]:
             img = Image.open(filePath+'/'+file_item)
             img = block_reduce(np.array(img), block_size=(DOWNSAMPLE_COEFFICIENT, DOWNSAMPLE_COEFFICIENT), func=np.mean)
             
@@ -92,7 +93,7 @@ def LoadImage():
 
 def classify(test, X_train, delta):
     X_hat = SOCP(test, X_train, Epsilon)
-    print "X_hat shape", X_hat.shape
+    # print "X_hat shape", X_hat.shape
     X_hat = np.array(X_hat)
     # for i in xrange(CLASSES):
     #     delta_i = np.zeros((CLASSES*SAMPLE_EACH_CLASS))
@@ -118,7 +119,7 @@ def classify(test, X_train, delta):
 def testAlgo():
     X_train, delta = LoadImage()
     # Current Path
-    currPath = '/Users/LeonGong/Desktop/ELEN6886/CroppedYale/'
+    currPath = '/Volumes/Myspace/Courses/sparse/project/Code-Lecture1/FaceIntroDemo/CroppedYale/'
     # Load the first image
 
     os.chdir(currPath)
@@ -126,32 +127,39 @@ def testAlgo():
 
 
     # Generate the random face
-    R_matrix = np.random.randn(100, X_train.T.shape[1])
+    R_matrix = np.random.randn(REDUCEDDIMENSION, X_train.T.shape[1])
     X_train = np.dot(R_matrix, X_train)
-    print 'R matrix', R_matrix.shape
-    print 'X matrix', X_train.shape
+    # print 'R matrix', R_matrix.shape
+    # print 'X matrix', X_train.shape
 
+    wrongSum = 0
+    loop1 = 0
     # Load images from different classes
     for i in range(len(classDirectory)):
         # List all the class directories
         filePath = currPath + classDirectory[i]
         os.chdir(filePath)
         fileList = glob.glob("*.pgm")
+        for loop in xrange(1, 10):
+            loop1 += 1
+            img = Image.open(filePath+'/'+fileList[loop])
+            # print fileList[loop] 
+            img = block_reduce(np.array(img), block_size=(DOWNSAMPLE_COEFFICIENT, DOWNSAMPLE_COEFFICIENT), func=np.mean)
+            
+            # Normalization
+            # img = img/np.sqrt(np.sum(img**2))
+            img = (img-np.mean(img))/np.std(img)
+            img = np.ndarray.flatten((np.array(img)))
 
-        img = Image.open(filePath+'/'+fileList[1])
-        print fileList[1] 
-        img = block_reduce(np.array(img), block_size=(DOWNSAMPLE_COEFFICIENT, DOWNSAMPLE_COEFFICIENT), func=np.mean)
-        
-        # Normalization
-        # img = img/np.sqrt(np.sum(img**2))
-        img = (img-np.mean(img))/np.std(img)
-        img = np.ndarray.flatten((np.array(img)))
+            # Calculate y_hat = Ry
+            img = np.dot(R_matrix, img)
 
-        # Calculate y_hat = Ry
-        img = np.dot(R_matrix, img)
+            predictLabel = classify(img.T, X_train, delta)
+            # print 'The prediction result:', predictLabel
+            # print 'Correct label: ', i
+            wrongSum += (predictLabel != i)
+    print float(wrongSum)/loop1
 
-        predictLabel = classify(img.T, X_train, delta)
-        print 'The prediction result:', predictLabel
 
 if __name__=='__main__':
     print(testAlgo())
